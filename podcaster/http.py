41720b331609defc82eb8aa1f9e60dffc6f8e4e6
Matthew Suozzo <matthew.suozzo@gmail.com>
2015-05-29 19:59:27 -0400

    fixed spacing, small changes to rss

--- podcaster/http.py
+++ podcaster/http.py
@@ -4,33 +4,33 @@ from bs4 import BeautifulSoup
 
 
 def get_soup(url):
-  #TODO: throw Exception?
-  try:
-    page = urllib2.urlopen(url)
-  except urllib2.HTTPError:
-    return None
-  return BeautifulSoup(page.read(), "lxml")
+   #TODO: throw Exception?
+   try:
+      page = urllib2.urlopen(url)
+   except urllib2.HTTPError:
+      return None
+   return BeautifulSoup(page.read(), "lxml")
 
 
 def _meta_redirect(soup):
-  meta = soup.find("meta", {"http-equiv": "refresh"})
-  return meta["content"].split("=")[1].strip() if meta else None
+   meta = soup.find("meta", {"http-equiv": "refresh"})
+   return meta["content"].split("=")[1].strip() if meta else None
 
 
 def get_meta_redirect(url):
-  soup = get_soup(url)
-  if soup is None:
-    return None
-  return _meta_redirect(soup)
+   soup = get_soup(url)
+   if soup is None:
+      return None
+   return _meta_redirect(soup)
 
 
 def _rss_link(soup):
-  link = soup.find("link", {"type": "application/rss+xml"})
-  return link["href"] if link else None
+   link = soup.find("link", {"type": "application/rss+xml"})
+   return link["href"] if link else None
 
 
 def get_rss_link(url):
-  soup = get_soup(url)
-  if soup is None:
-    return None
-  return _rss_link(soup)
+   soup = get_soup(url)
+   if soup is None:
+      return None
+   return _rss_link(soup)
--- podcaster/rss.py
+++ podcaster/rss.py
@@ -1,52 +1,61 @@
-from http import get_meta_redirect, get_rss_link
+from podcaster.http import get_meta_redirect, get_rss_link
 
-from time import time
+from datetime import datetime
+import time
 
 import feedparser
 
 
 class Podcast(object):
-  """A podcast feed
-  """
+    """A podcast feed
+    """
 
-  def __init__(self, url, **kwargs):
-    self.url = url
-    self.author = kwargs.get('author', '')
-    self.link = kwargs.get('link', '')
-    self.summary = kwargs.get('summary', '')
-    self.title = kwargs.get('title', '')
-    self.last_updated = kwargs.get('updated_parsed', time())
+    def __init__(self, url, **kwargs):
+        self.url = url
+        self.title = kwargs.get('title', self.url)
+        self.author = kwargs.get('author', '')
+        self.site_link = kwargs.get('link', '')
+        self.description = kwargs.get('summary', '')
 
-  def __str__(self):
-    return '"%s" By %s' % (self.title, self.author)
+        #FIXME: choose good default for unmarked podcasts
+        iso_updated = kwargs.get('updated_parsed', time.localtime())
+        self.last_updated = datetime(*iso_updated[:7])
+
+    def __str__(self):
+        return '"%s" By %s' % (self.title, self.author)
 
 
 def parse_feed(url):
-  # first try to parse directly
-  parsed_feed = feedparser.parse(url)
-  # fall back to searching for an rss link
-  if parsed_feed.bozo:
-    #TODO: Generalize fallbacks
-    # try to find an rss_link
-    rss_link = get_rss_link(url)
-    if rss_link is not None:
-      return parse_feed(rss_link)
-    # try to find a redirect within the page
-    redirect_url = get_meta_redirect(url)
-    if redirect_url is not None:
-      return parse_feed(redirect_url)
-    return None
-  else:
-    return parsed_feed
+    """Return feed structured via feedparser on success.
+    None on failure
+    """
+    # first try to parse directly
+    parsed_feed = feedparser.parse(url)
+
+    # fall back to searching for an rss link
+    if parsed_feed.bozo:
+        #TODO: Generalize fallbacks
+        # try to find an rss_link
+        rss_link = get_rss_link(url)
+        if rss_link is not None:
+            return parse_feed(rss_link)
+
+        # try to find a redirect within the page
+        redirect_url = get_meta_redirect(url)
+        if redirect_url is not None:
+            return parse_feed(redirect_url)
+        return None
+    else:
+        return parsed_feed
 
 
 if __name__ == "__main__":
-  import pprint
+    import pprint
 
-  hi_url = "http://feeds.podtrac.com/m2lTaLRx8AWb"
-  shorten = lambda s: str(s)[:40] + ("..." if len(str(s)) > 40 else "")
-  feed = parse_feed(hi_url)
-  for k, v in feed.iteritems():
-    print k, shorten(v)
-  print pprint.pprint(feed.feed)
-  p = Podcast(feed.href, **feed.feed)
+    hi_url = "http://feeds.podtrac.com/m2lTaLRx8AWb"
+    shorten = lambda s: str(s)[:40] + ("..." if len(str(s)) > 40 else "")
+    feed = parse_feed(hi_url)
+    for k, v in feed.iteritems():
+        print k, shorten(v)
+    print pprint.pprint(feed.feed)
+    p = Podcast(feed.href, **feed.feed)
