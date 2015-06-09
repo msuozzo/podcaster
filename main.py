c37a3cab5cb3b27dee01f5bfda1262895bc27ccc
Matthew Suozzo <matthew.suozzo@gmail.com>
2015-06-09 16:01:04 -0400

    fixed python executable in main script, added default for vlc plugin path var

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/env python  #pylint: disable=missing-docstring
 from podcaster.main import Podcaster
 
+import os
+
+
+os.environ.setdefault('VLC_PLUGIN_PATH', '/Applications/VLC.app/Contents/MacOS/plugins/')
 Podcaster().run()
