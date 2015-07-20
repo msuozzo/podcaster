12d50044cce89868d7ddb0785a7e8b6150898eb0
Matthew Suozzo <matthew.suozzo@gmail.com>
2015-07-20 19:50:08 -0400

    Added connection failure handling, docstrings

--- podcaster/http.py
+++ podcaster/http.py
@@ -37,7 +37,9 @@ def download_to_file(url, fname=None, reporthook=None):
     try:
         return urlretrieve(url, fname, reporthook)
     except IOError as err:
-        raise ConnectionError('Failed to establish connection: %s' % err)
+        if not test_connection():
+            raise ConnectionError('No connection')
+        raise ResponseError('Error during download: %s' % err)
 
 
 def default_to_http(url):
@@ -61,8 +63,20 @@ def test_connection():
 
 def get_soup(url):
     """Return a BeautifulSoup instance for the contents of `url`
+
+    Raises:
+        ConnectionError: If no internet connection detected
+        ResponseError: If an error occurs in another process
     """
-    return BeautifulSoup(urlopen(url).read(), 'lxml')
+    try:
+        response = urlopen(url).read()
+    except ValueError as err:
+        raise ResponseError(str(err))
+    except (HTTPError, URLError) as err:
+        if not test_connection():
+            raise ConnectionError('No connection')
+        raise ResponseError(str(err.reason))
+    return BeautifulSoup(response, 'lxml')
 
 
 def _meta_redirect(soup):
@@ -77,11 +91,7 @@ def get_meta_redirect(url):
     """If one exists, return the meta redirect url for the page at `url`.
     Else, return None.
     """
-    try:
-        soup = get_soup(url)
-    except HTTPError:
-        soup = None
-    return _meta_redirect(soup) if soup is not None else None
+    return _meta_redirect(get_soup(url))
 
 
 def _rss_link(soup):
@@ -96,11 +106,7 @@ def get_rss_link(url):
     """If one exists, return the RSS url for the page at `url`.
     Else, return None.
     """
-    try:
-        soup = get_soup(url)
-    except HTTPError:
-        soup = None
-    return _rss_link(soup) if soup is not None else None
+    return _rss_link(get_soup(url))
 
 
 if socket.getdefaulttimeout() is None:
--- podcaster/operations.py
+++ podcaster/operations.py
@@ -1,9 +1,7 @@
-"""
-"""
 from podcaster.model import Podcast, Episode, EpisodeFile, BaseModel
 from podcaster.rss import get_podcast
 from podcaster.store import SimplerFileStore
-from podcaster.http import download_to_file, ConnectionError
+from podcaster.http import download_to_file, ConnectionError, ResponseError
 from podcaster.view import ASCIIView
 
 from contextlib import contextmanager
@@ -37,7 +35,11 @@ class Controller(object):
 
     @contextmanager
     def session(self):
-        """Context manager for 
+        """Context manager for database session
+
+        On enter: Create a new db session (provided one is not already in
+            progress)
+        On exit: Commit the session (provided one was created)
         """
         new_session = self._session is None
         if new_session:
@@ -82,22 +84,31 @@ class Controller(object):
 
     @_with_session
     def update_podcasts(self, cb_return_menu):
-        self.view.update()
+        self.view.update(start=True)
         podcasts = self._session.query(Podcast).all()
         for podcast in podcasts:
-            self._update_podcast(podcast)
+            try:
+                self._update_podcast(podcast)
+            except (ConnectionError, ResponseError) as err:
+                self.view.update(error=(podcast, 'Failed to connect to update \
+                    server (%s)' % str(err)))
+        self.view.update(end=True)
         return cb_return_menu
 
     @_with_session
     def update_podcast(self, podcast_id, cb_return_menu):
+        self.view.update(start=True)
         podcast = self._session.query(Podcast).get(podcast_id)
-        self._update_podcast(podcast)
+        try:
+            self._update_podcast(podcast)
+        except (ConnectionError, ResponseError) as err:
+            self.view.update(error=(podcast, 'Failed to connect to update \
+                server (%s)' % str(err)))
+        self.view.update(end=True)
         return cb_return_menu
 
     def _update_podcast(self, podcast):
         podcast_tuple, episode_iter = get_podcast(podcast.rss_url)
-        if podcast_tuple is None:
-            raise Exception()
         _, _, last_updated, _, _, _ = podcast_tuple
         if last_updated is None or \
                 last_updated.replace(tzinfo=None) <= podcast.last_updated:
@@ -121,20 +132,42 @@ class Controller(object):
             self._session.delete(episode)
 
     def get_podcast_name(self, podcast_url):
-        podcast_tuple, _ = get_podcast(podcast_url)
+        """Return the name of the podcast referred to by `podcast_url`
+
+        Returns:
+            On success: The name of the podcast
+            On HTTP failure: A 2-tuple of the form (None, str_reason)
+            On RSS failure: None
+        """
+        try:
+            podcast_tuple, _ = get_podcast(podcast_url)
+        except (ConnectionError, ResponseError) as err:
+            return (None, str(err))
         if podcast_tuple is None:
             return None
         title, _, _, _, _, _ = podcast_tuple
         return title
 
     def add_podcast(self):
+        """Dummy operation to transfer control to the view `add_podcast`
+        """
         return self.view.add_podcast()
 
     @_with_session
     def new_podcast(self, podcast_url):
-        podcast_tuple, episode_iter = get_podcast(podcast_url)
-        if podcast_tuple is None:
-            return None
+        """Add a new podcast
+
+        Args:
+            podcast_url: The url of the podcast RSS feed to add
+
+        Returns:
+            On success: None
+            On failure: An error string
+        """
+        try:
+            podcast_tuple, episode_iter = get_podcast(podcast_url)
+        except (ConnectionError, ResponseError) as err:
+            return str(err)
         title, rss_url, last_updated, _, _, _ = podcast_tuple
         podcast = Podcast(name=title, rss_url=rss_url, last_updated=last_updated)
         self._session.add(podcast)
@@ -146,6 +179,16 @@ class Controller(object):
 
     @_with_session
     def play(self, episode_id, cb_return_menu):
+        """Play an episode
+
+        Args:
+            episode_id: The id of the episode to attempt to play
+            cb_return_menu: The menu callback to invoked after the play
+                operation is complete.
+                NOTE: The operation is 'complete' when a triggered download
+                fails, the user exits the player, or the episode finishes
+                playback
+        """
         episode = self._session.query(Episode).get(episode_id)
         podcast = self._session.query(Podcast).get(episode.podcast_id)
         if not episode.is_downloaded() and not self.view.download(episode):
@@ -154,13 +197,27 @@ class Controller(object):
 
     @_with_session
     def download_episode(self, episode_id, cb_progress=None):
+        """Attempt to download an episode
+
+        Args:
+            episode_id: The id of the episode to be downloaded
+            cb_progress: An optional callback function that will be called as
+                frequently as possible with the completion ratio of the
+                download.
+                NOTE: If this information is not available, the callback will
+                be called with `None`
+
+        Return:
+            On Success: None
+            On Failure: An error string
+        """
         episode = self._session.query(Episode).get(episode_id)
         key = self._episode_key(episode_id)
         # Define callback closure to convert download callback format to progress format
         if cb_progress is not None:
             def cb_report(chunk_num, chunk_size, total_size):
-                """Return the completion ratio of the download if available.
-                Else, return None
+                """Call `cb_progress` with the completion ration if available.
+                Else, call it with `None`
                 """
                 if total_size != -1:
                     cb_progress((1. * chunk_size * chunk_num) / total_size)
@@ -171,17 +228,24 @@ class Controller(object):
         # Attempt download
         try:
             local_fname, _ = download_to_file(episode.url, reporthook=cb_report)
-        except ConnectionError:
-            return False
+        except (ConnectionError, ResponseError) as err:
+            return str(err)
         else:
             with open(local_fname, 'rb') as file_:
                 self._store.put(key, file_)
             uri = 'file://' + self._store.get_path(key)
             episode.local_file = EpisodeFile(episode_id=episode_id, uri=uri)
-            return True
+            return None
 
     @_with_session
     def delete_episode(self, episode_id, cb_return_menu):
+        """Delete an episode
+
+        Args:
+            episode_id: The id of the episode to be deleted
+            cb_return_menu: The menu callback to be returned upon completion
+                of the operation.
+        """
         episode = self._session.query(Episode).get(episode_id)
         key = self._episode_key(episode_id)
         self._store.remove(key)
@@ -196,6 +260,12 @@ class Controller(object):
         self._session.flush()
 
     def _episode_key(self, episode_id):
+        """Return a (reasonably) unique string identifier for an episode
+
+        Args:
+            episode_id: The id of the episode for which the string identifier
+                should be generated.
+        """
         episode = self._session.query(Episode).get(episode_id)
         podcast = self._session.query(Podcast).get(episode.podcast_id)
         return ' - '.join((podcast.name, episode.title))
